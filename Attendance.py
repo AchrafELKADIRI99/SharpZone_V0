bdcd68757bbeda1411735ce6dd056021a19cb222
AchrafELKADIRI99 <achrafelkadiri90@gmail.com>
2021-12-12 02:47:02 +0100

    added attendance script to detect and save names and detection time in a csv file

--- Attendance.py
+++ Attendance.py
@@ -2,27 +2,29 @@ import cv2
 import face_recognition
 import numpy as np
 import os
+from datetime import datetime
 
-#define folder
-path='ImagesAttendance'
+# define folder
+path = 'ImagesAttendance'
 
-#list of images
-images=[]
+# list of images
+images = []
 
-#names of people getting them from images filenames
-classNames=[]
+# names of people getting them from images filenames
+classNames = []
 
-#getting images
-myList=os.listdir(path)
+# getting images
+myList = os.listdir(path)
 
-#importing them using opencv function
+# importing them using opencv function
 for cl in myList:
     currentImage = cv2.imread(f'{path}/{cl}')
     images.append(currentImage)
     classNames.append(os.path.splitext(cl)[0])
 print(classNames)
 
-#function to encode all images
+
+# function to encode all images
 def findEncodings(images):
     encodeList = []
     for img in images:
@@ -31,43 +33,62 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
-#encode all images
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList=f.readlines()
+        nameList=[]
+        # print(myDataList)
+        for line in myDataList:
+            #split them based on ,
+            entry=line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now=datetime.now()
+            dateString=now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dateString}')
+
+
+
+
+
+# encode all images
 encodeListKnown = findEncodings(images)
 print("Encoding Completed")
 
-#initialize camera
-cap=cv2.VideoCapture(0)
+# initialize camera
+cap = cv2.VideoCapture(0)
 
-#send every frame (capture)
+# send every frame (capture)
 while True:
     success, img = cap.read()
-    #resize pictures
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    # resize pictures
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     facesCurrentFrame = face_recognition.face_locations(imgS)
     encodesCurrentFrame = face_recognition.face_encodings(imgS, facesCurrentFrame)
 
-    for encodeFace,faceLoc in zip(encodesCurrentFrame,facesCurrentFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)
+    for encodeFace, faceLoc in zip(encodesCurrentFrame, facesCurrentFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
         print(faceDis)
-        #use umpy to know the most close value to encodes
-        matchIndex=np.argmin(faceDis)
+        # use numpy to know the most close value to encodes
+        matchIndex = np.argmin(faceDis)
 
-        #display box with name
+        # display box with name
         if matches[matchIndex]:
-            name=classNames[matchIndex].upper()
-            print (name)
+            name = classNames[matchIndex].upper()
+            print(name)
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            markAttendance(name)
 
-    cv2.imshow('webcam',img)
+
+    cv2.imshow('webcam', img)
     cv2.waitKey(1)
-
 
 # # locate face1
 # faceLoc1 = face_recognition.face_locations(imgElon1)[0]
